182f92e0923d91940ad0ad8d0d802b7f8a235349
Merlin Webster <merlin.webster@multiverse.io>
2024-12-20 20:48:28 +0000

    Update README

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,9 +2,8 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { UserList } from "./components/UserList";
 import { AddUserForm } from "./components/AddUserForm";
-
+import { UserList } from "./components/UserList";
 function App() {
   const [count, setCount] = useState(0);
 
