e2ed0da7a33ce337e4c35f08de117f782a4b99a6
Merlin Webster <mjftwebster@gmail.com>
2024-12-26 19:39:59 +0100

    Swap more tests over to new test helper

--- apps/api/src/controllers/app.controller.spec.ts
+++ apps/api/src/controllers/app.controller.spec.ts
@@ -1,18 +1,11 @@
 import { describe, expect } from 'vitest';
 import { AppController } from './app.controller';
 import { AppService } from '../app.service';
-import { ConfigModule } from '@nestjs/config';
-import { validateEnv } from '../config/environment-variables';
+
 import { createModuleTest } from 'test/utils/vitest';
 
 describe('AppController', () => {
   const test = createModuleTest({
-    imports: [
-      ConfigModule.forRoot({
-        isGlobal: true,
-        validate: () => validateEnv({ DATABASE_URL: 'test' }),
-      }),
-    ],
     controllers: [AppController],
     providers: [AppService],
   });
--- apps/api/test/utils/vitest.ts
+++ apps/api/test/utils/vitest.ts
@@ -4,13 +4,13 @@ import { Test } from '@nestjs/testing';
 import { test } from 'vitest';
 
 export function createModuleTest(
-  metadata: Parameters<typeof Test.createTestingModule>[0],
+  ...createTestingModuleArgs: Parameters<typeof Test.createTestingModule>
 ) {
   const moduleTest = test.extend<{ module: TestingModule }>({
     module: async ({}, use) => {
-      const module = await Test.createTestingModule({
-        ...metadata,
-      }).compile();
+      const module = await Test.createTestingModule(
+        ...createTestingModuleArgs,
+      ).compile();
 
       return use(module);
     },
