9bd051ddbfc5ad7deffa5716331b08e67006d573
Merlin Webster <mjftwebster@gmail.com>
2024-12-26 23:55:12 +0100

    Add a beforeEach and afterEach function to createModuleTest

--- apps/api/test/utils/vitest.ts
+++ apps/api/test/utils/vitest.ts
@@ -1,20 +1,34 @@
 import { TestingModule } from '@nestjs/testing';
 
 import { Test } from '@nestjs/testing';
-import { test } from 'vitest';
+import { test, afterEach } from 'vitest';
 
 export function createModuleTest(
-  ...createTestingModuleArgs: Parameters<typeof Test.createTestingModule>
+  createTestingModuleArg: Parameters<typeof Test.createTestingModule>[0],
+  options?: {
+    beforeEach?: ({ module }: { module: TestingModule }) => Promise<void>;
+    afterEach?: ({ module }: { module: TestingModule }) => Promise<void>;
+  },
 ) {
   const moduleTest = test.extend<{ module: TestingModule }>({
     module: async ({}, use) => {
       const module = await Test.createTestingModule(
-        ...createTestingModuleArgs,
+        createTestingModuleArg,
       ).compile();
 
       return use(module);
     },
   });
+
+  options?.beforeEach &&
+    beforeEach(async ({ module }: { module: TestingModule }) => {
+      await options.beforeEach?.({ module });
+    });
+
+  options?.afterEach &&
+    afterEach(async ({ module }: { module: TestingModule }) => {
+      await options.afterEach?.({ module });
+    });
 
   return moduleTest;
 }
