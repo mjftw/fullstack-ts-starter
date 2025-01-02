ab81da5847eaf5be5161a8ebbd9101781539f98e
Merlin Webster <mjftwebster@gmail.com>
2025-01-02 14:14:35 +0100

    Add missing tRPC deps to web-ssr

--- apps/web-ssr/src/entry-server.tsx
+++ apps/web-ssr/src/entry-server.tsx
@@ -1,15 +1,18 @@
-import { StrictMode } from 'react'
+import { StrictMode } from "react";
 import {
   type RenderToPipeableStreamOptions,
   renderToPipeableStream,
-} from 'react-dom/server'
-import App from './App'
+} from "react-dom/server";
+import App from "./App";
+import { TRPCProvider } from "./utils/trpc";
 
 export function render(_url: string, options?: RenderToPipeableStreamOptions) {
   return renderToPipeableStream(
     <StrictMode>
-      <App />
+      <TRPCProvider apiURL="http://localhost:3000/api/trpc">
+        <App />
+      </TRPCProvider>
     </StrictMode>,
-    options,
-  )
+    options
+  );
 }
