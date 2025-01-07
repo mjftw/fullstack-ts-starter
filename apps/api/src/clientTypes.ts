10d1a8807f571ba076ff2cd1859f4ff9ac819ecc
Merlin Webster <mjftwebster@gmail.com>
2025-01-07 10:24:13 +0000

    WIP Add client types ready for import in frontend

---
+++ apps/api/src/clientTypes.ts
@@ -0,0 +1,4 @@
+// This file is imported as types only on the client in order to provide type safety
+// at the client-server interfaces.
+export { type RenderFn } from './reactSSR/reactSSR.service';
+export { type AppRouter } from './trpc/routers';
