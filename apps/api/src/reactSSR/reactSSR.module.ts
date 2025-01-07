005071f98175bcfb722eb6d7c239deeb7b593387
Merlin Webster <merlin.webster@multiverse.io>
2025-01-07 18:09:40 +0000

    Improve comments

--- apps/api/src/reactSSR/reactSSR.module.ts
+++ apps/api/src/reactSSR/reactSSR.module.ts
@@ -14,16 +14,10 @@ import { ConfigService } from '@nestjs/config';
   controllers: [ReactSSRController],
 })
 export class ReactSSRModule implements NestModule {
-  // FIXME: Currently we have an issue where the static files middleware and the react SSR controller
-  // are both listening to the same path.
-  // This works okay since the ReactSSRController takes priority, but it's not a clean solution.
-  // Also if the rendering the app fails, the static files middleware is be used instead.
-  // This is very confusing as the static files middleware then picks up the index.html file
-  // and serves it statically, without replacing the app content in the document, giving what appears
-  // to be a correct render, but with a blank page.
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(StaticMiddleware)
+      // Match only files rather than routes (assuming files have a . extension - e.g. vite.svg)
       .forRoutes({ path: '*.*', method: RequestMethod.GET });
   }
 
