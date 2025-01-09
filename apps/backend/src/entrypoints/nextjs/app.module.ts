6d09520af3407bddecdcf0de9182f0aad2078f4b
Merlin Webster <merlin.webster@multiverse.io>
2025-01-09 14:52:19 +0000

    Remove unused import

--- apps/backend/src/entrypoints/nextjs/app.module.ts
+++ apps/backend/src/entrypoints/nextjs/app.module.ts
@@ -12,8 +12,6 @@ import { ServicesModule } from '~/services/services.module';
 import { TrpcMiddleware } from '~/middlewares/trpc.middleware';
 import { LoggerMiddleware } from '~/middlewares/logging.middleware';
 
-import { StaticMiddleware } from '~/middlewares/static.middleware';
-
 @Module({
     imports: [
         ConfigModule.forRoot({
